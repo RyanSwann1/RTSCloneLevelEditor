2cf15eb33243b82638e8f885398c19365f8b97f7
Ryan Swann <RyanSwann1992@gmail.com>
2020-09-05 17:02:46 +0100

    Initial SFML-Window creation

--- RTSCloneLevelEditor/RTSCloneLevelEditor/main.cpp
+++ RTSCloneLevelEditor/RTSCloneLevelEditor/main.cpp
@@ -1,8 +1,44 @@
-#include <iostream>
+#include "glm/glm.hpp"
+#include "glad.h"
+#include <SFML/Graphics.hpp>
 
 int main()
 {
-	std::cout << "Hello World\n";
+	sf::ContextSettings settings;
+	settings.depthBits = 24;
+	settings.stencilBits = 8;
+	settings.antialiasingLevel = 4;
+	settings.majorVersion = 3;
+	settings.minorVersion = 3;
+	settings.attributeFlags = sf::ContextSettings::Core;
+	glm::uvec2 windowSize(1280, 800);
+	sf::Window window(sf::VideoMode(windowSize.x, windowSize.y), "RTS Clone", sf::Style::Default, settings);
+	window.setFramerateLimit(60);
+	window.setMouseCursorGrabbed(true);
+	gladLoadGL();
+	
+	while (window.isOpen())
+	{
+		sf::Event currentSFMLEvent;
+		while (window.pollEvent(currentSFMLEvent))
+		{
+			if (currentSFMLEvent.type == sf::Event::Closed)
+			{
+				window.close();
+			}
+			else if (currentSFMLEvent.type == sf::Event::KeyPressed)
+			{
+				if (currentSFMLEvent.key.code == sf::Keyboard::Escape)
+				{
+					window.close();
+				}
+			}
+		}
+
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+		window.display();
+	}
 
 	return 0;
 }
